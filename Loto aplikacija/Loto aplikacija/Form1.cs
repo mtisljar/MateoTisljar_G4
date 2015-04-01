2df3cdcb8c1eb376b484e5190552018200fcbf23
MateoTisljar <mtisljar@foi.hr>
2015-04-01 10:51:06 +0200

    Izrađeno grafičko sučelje aplikacije

--- Loto aplikacija/Loto aplikacija/Form1.cs
+++ Loto aplikacija/Loto aplikacija/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Loto_aplikacija
 {
-    public partial class Form1 : Form
+    public partial class FrmLoto : Form
     {
-        public Form1()
+        public FrmLoto()
         {
             InitializeComponent();
         }
